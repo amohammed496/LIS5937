a50298a395fbd8fc417d47260d6fae38e32deeb7
amohammed496 <77594806+amohammed496@users.noreply.github.com>
2021-03-22 01:27:49 -0400

    Add files via upload

---
+++ module_10_pkg/R/module_10_demo_source.R
@@ -0,0 +1,18 @@
+#'Source code for module 10 demo package
+#'
+#'@param x numerical value
+#'
+#'@return returns the given x value multiplied by itself
+#'
+#'@examples
+#'testFunction(5)
+#'testFunction(9)
+
+testFunction = function(x)
+{
+
+  y = x * x
+  return(y)
+
+}
+
